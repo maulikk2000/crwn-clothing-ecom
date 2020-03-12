bdd6252fba384927ff0105388b47f51075595f35
Maulik Khandwala <maulikk2000@gmail.com>
2020-03-13 01:47:58 +1100

    user signin changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,44 @@ import { firebaseauth } from "./firebase/firebase.utils";
 function App() {
   let initialState: any;
   const [currentUser, setcurrentUser] = useState(initialState);
-
+  let unsubscribeFromAuth = null;
   useEffect(() => {
-    firebaseauth.onAuthStateChanged(user => {
+    unsubscribeFromAuth = firebaseauth.onAuthStateChanged(user => {
       setcurrentUser(user);
+      console.log("user is", user);
       console.log("current user is ", currentUser?.["email"]);
+      //console.log("current user is ", currentUser);
     });
 
+    unsubscribeFromAuth();
     // return () => {
     //   cleanup
     // };
-  });
+  }, [currentUser]);
 
   return (
     <div>
       {/* <HomePage></HomePage> */}
+      {/* <div>{currentUser?.["displayName"]}</div> */}
+      {/* <div>
+        {currentUser
+          ? currentUser.providerData.forEach(
+              (profile: {
+                providerId: string;
+                uid: string;
+                displayName: string;
+                email: string;
+                photoURL: string;
+              }) => {
+                console.log("Sign-in provider: " + profile.providerId);
+                console.log("  Provider-specific UID: " + profile.uid);
+                console.log("  Name: " + profile.displayName);
+                console.log("  Email: " + profile.email);
+                console.log("  Photo URL: " + profile.photoURL);
+              }
+            )
+          : null}
+      </div> */}
       <HeaderComponent />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
